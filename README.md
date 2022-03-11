848729b6415fac945f9f404cf351885a4738b66d
Cutler Sheridan <cutler.sheridan@gmail.com>
2022-03-10 18:05:57 -0800

    0.0.2 Generate General page; toggle isComplete upon clicking checkbox

--- README.md
+++ README.md
@@ -7,11 +7,17 @@ If you need a free place to keep track of your to-do list, look no further.
 #### TO-DO LATER
 ##### Features
 ##### Behavior
+- should tasks get auto-added to taskArray?
+- auto-assign task.completionDateTime if it's marked complete
 ##### Style
 ##### Other
 ##### Maybe
 
 #### DONE
+- *0.0.2*
+- write code to generate General page
+- create function to toggle appearance of completed tasks
+- create function to toggle tasks' isComplete property upon clicking the checkbox
 - *0.0.1*
 - create basic Index page
 - add light styling to Index
--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const model = (() => {
         let notes = null;
         let checklist = null
         let isComplete = false;
-        let completionDate = null;
+        let completionDateTime = null;
         // let timeCreated = currentTime;
         return {
             name,
@@ -18,7 +18,7 @@ const model = (() => {
             notes,
             checklist,
             isComplete,
-            completionDate,
+            completionDateTime,
             // timeCreated,
         }
     }
@@ -33,17 +33,78 @@ const model = (() => {
 })();
 
 const controller = (() => {
-    // add code
+    const _createTask = (name) => {
+        return model.Task(name);
+    }
+    const _addTaskToArray = (task) => {
+        model.taskArray.push(task);
+    }
+    const addNewTask = (name) => {
+        const task = _createTask(name);
+        _addTaskToArray(task);
+    }
+    // const _createProject = (name) => {
+    //     return model.Project(name);
+    // }
+    // const _addProjectToArray = (project) => {
+    //     model.projectArray.push(project);
+    // }
+    // const addNewProject = (name) => {
+    //     const project = _createProject(name);
+    //     _addProjectToArray(project);
+    // }
+    const toggleTaskCompletion = (task) => {
+        task.isComplete = !task.isComplete;
+        if (task.isComplete) {
+            // task.completionDateTime = currentTime;
+        } else {
+            task.completionDateTime = null;
+        }
+    }
+    return {
+        addNewTask,
+        toggleTaskCompletion,
+    }
 })();
 
 const view = (() => {
-    const createTaskElement = (task) => {
+    const _contentDiv = document.querySelector(".content");
+    const createGeneralPage = () => {
+        // create header
+        const heading = document.createElement("h1");
+        heading.textContent = "General";
+        _contentDiv.append(heading);
+        _updateTaskList();
+    }
+    const _updateTaskList = () => {
+        const taskListDiv = document.createElement("section");
+        const incompleteTasks = document.createElement("section");
+        incompleteTasks.classList.add("task-list");
+        const completeTasks = document.createElement("section");
+        completeTasks.classList.add("task-list");
+        taskListDiv.append(incompleteTasks, completeTasks);
+        _contentDiv.append(taskListDiv);
+
+        model.taskArray.forEach(task => {
+            if (task.isComplete) {
+                completeTasks.append(_createTaskElement(task));
+            } else {
+                incompleteTasks.append(_createTaskElement(task));
+            }
+        })
+    }
+    const _createTaskElement = (task) => {
         const taskContainer = document.createElement("div");
         taskContainer.classList.add("task-container");
+        taskContainer.dataset.task = model.taskArray.indexOf(task);
         const taskLabel = document.createElement("label");
         taskLabel.classList.add("task-label");
+        taskLabel.dataset.task = model.taskArray.indexOf(task);
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
+        checkbox.dataset.task = model.taskArray.indexOf(task);
+        checkbox.addEventListener("change", _toggleCompleteClass);
+
         taskLabel.append(checkbox);
         taskLabel.append(task.name);
         taskContainer.append(taskLabel);
@@ -53,18 +114,32 @@ const view = (() => {
             dueDateElement.textContent = task.dueDate;
             taskContainer.append(dueDateElement);
         }
+        if (task.isComplete) {
+            checkbox.checked = true;
+            taskContainer.classList.add("complete-task");
+        }
         return taskContainer;
     }
-    return {createTaskElement}
+    const _toggleCompleteClass = (e) => {
+        const taskIndex = e.target.dataset.task;
+        const taskContainer = document.querySelector(`.task-container[data-task="${taskIndex}"]`);
+        taskContainer.classList.toggle("complete-task");
+        controller.toggleTaskCompletion(model.taskArray[taskIndex]);
+        console.log(model.taskArray[taskIndex]);
+    }
+    return {
+        createGeneralPage,
+    }
 })();
 
 const setup = (() => {
     model.taskArray.push(
+        {name: "First test", dueDate: "7/14"},
+        {name: "This is the second test"},
         {name: "This is a third test", dueDate: "5/15"},
-        {name: "This is a fourth test", dueDate: "6/16"},
+        {name: "This is a fourth test", dueDate: "6/16", isComplete: true},
     )
     const content = document.querySelector(".content");
-    model.taskArray.forEach(task => {
-        content.append(view.createTaskElement(task));
-    })
+
+    view.createGeneralPage();
 })();
